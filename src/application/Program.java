f360b4a69be541f6b006bd96a960174d16902e9c
matheusvidal21 <matheusvidal140@gmail.com>
2023-07-18 08:41:04 -0300

    Starting to implement Board and piece

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,7 +1,17 @@
 package application;
 
+import boardgame.Board;
+
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Program {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        Board board = new Board(8,8);
+
+        sc.close();
     }
 }
